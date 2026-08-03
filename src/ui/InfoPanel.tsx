7e8dc02fd4dd37b7e3d6952c7f99fe352772620e
Bart <b0rgbart3@gmail.com>
2026-08-03 09:02:05 -0700

    adding orrery example image to about modal

--- src/ui/InfoPanel.tsx
+++ src/ui/InfoPanel.tsx
@@ -26,29 +26,45 @@ export function InfoPanel({
     <>
       {aboutOpen && (
         <Modal title="About The Orrery" onClose={() => onAboutOpenChange(false)}>
-          <h3 className="modal__subtitle">What is an Orrery:</h3>
-          <p className="modal__paragraph">
-            An orrery is a mechanical model of the solar system that represents the relative
-            positions and motions of the planets as they orbit the sun. Historically built as
-            clockwork devices, orreries have long been used to visualize and teach the scale and
-            movement of our solar system in a way that is easy to observe and understand.
-          </p>
+          <div className="modal__intro">
+            <div className="modal__intro-block">
+              <h3 className="modal__subtitle">What is an Orrery:</h3>
+              <p className="modal__paragraph">
+                An orrery is a mechanical model of the solar system that represents the relative
+                positions and motions of the planets as they orbit the sun. Historically built as
+                clockwork devices, orreries have long been used to visualize and teach the scale
+                and movement of our solar system in a way that is easy to observe and understand.
+              </p>
+            </div>
 
-          <h3 className="modal__subtitle">How its setup:</h3>
-          <p className="modal__paragraph">
-            While the proportions of the sun and the planets are correct, the{" "}
-            <strong>distance</strong> between them has been greatly reduced so they fit
-            comfortably within this interface. This is an intentional fiction - in reality the
-            distances between planets are vastly greater than shown here.
-          </p>
+            <img
+              className="modal__intro-image"
+              src="/orrery_example.jpg"
+              alt="An antique mechanical orrery model"
+            />
 
-          <h3 className="modal__subtitle">What you can control:</h3>
-          <p className="modal__paragraph">
-            You can spin the entire solar system around by clicking and moving your mouse. You
-            can zoom in and out of the scene, by using the middle mouse button. You can click on
-            a planet to zoom into it and learn more details about that planet. To get back to the
-            normal starting orientation, just click on the background (stars).
-          </p>
+            <div className="modal__intro-block">
+              <h3 className="modal__subtitle">
+                How <strong><em>this Orrery</em></strong> is setup.
+              </h3>
+              <p className="modal__paragraph">
+                While the proportions of the sun and the planets are correct, the{" "}
+                <strong>distance</strong> between them has been greatly reduced so they fit
+                comfortably within this interface. This is an intentional fiction - in reality the
+                distances between planets are vastly greater than shown here.
+              </p>
+            </div>
+
+            <div className="modal__intro-block">
+              <h3 className="modal__subtitle">What you can control:</h3>
+              <p className="modal__paragraph">
+                You can spin the entire solar system around by clicking and moving your mouse. You
+                can zoom in and out of the scene, by using the middle mouse button. You can click
+                on a planet to zoom into it and learn more details about that planet. To get back
+                to the normal starting orientation, just click on the background (stars).
+              </p>
+            </div>
+          </div>
 
           <div className="modal__controls">
             <label className="modal__toggle">
